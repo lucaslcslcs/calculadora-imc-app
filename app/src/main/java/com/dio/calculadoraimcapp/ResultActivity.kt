a06d89570ae12feab7a9a8505f41055938a0338a
lucas <lucaslcslcs1998@gmail.com>
2022-07-23 13:11:14 -0300

    Componente Activity

--- app/src/main/java/com/dio/calculadoraimcapp/ResultActivity.kt
+++ app/src/main/java/com/dio/calculadoraimcapp/ResultActivity.kt
@@ -3,10 +3,9 @@ package com.dio.calculadoraimcapp
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 
-class MainActivity : AppCompatActivity() {
-
+class ResultActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        setContentView(R.layout.activity_result)
     }
 }
