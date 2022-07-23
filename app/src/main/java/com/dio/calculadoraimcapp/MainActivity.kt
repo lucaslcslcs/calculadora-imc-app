0f8f96bf87fff89c3349711555b1caee20e405ac
lucas <lucaslcslcs1998@gmail.com>
2022-07-23 13:53:04 -0300

    Ciclo de vida

--- app/src/main/java/com/dio/calculadoraimcapp/MainActivity.kt
+++ app/src/main/java/com/dio/calculadoraimcapp/MainActivity.kt
@@ -2,6 +2,7 @@ package com.dio.calculadoraimcapp
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Log
 
 class MainActivity : AppCompatActivity() {
 
@@ -9,4 +10,30 @@ class MainActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
     }
+
+    override fun onStart() {
+        super.onStart()
+        Log.w("lifecycle","Start - deixei a tela visivel para você")
+    }
+
+    override fun onResume() {
+        super.onResume()
+        Log.w("lifecycle","Resume - agora você pode interagir com a tela")
+    }
+
+    override fun onPause() {
+        super.onPause()
+        Log.w("lifecycle","Pause - a tela perdeu o foco, você não pode mais interagir")
+    }
+
+    override fun onStop() {
+        super.onStop()
+        Log.w("lifecycle","Shop - a tela foi parada")
+    }
+
+    override fun onDestroy() {
+        super.onDestroy()
+        Log.w("lifecycle","Destroy - a tela foi destruida")
+    }
+
 }
