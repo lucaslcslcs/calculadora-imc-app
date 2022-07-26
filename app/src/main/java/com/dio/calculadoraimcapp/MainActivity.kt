5ae24e1f4b32b4b50e34ebb72376564e2367c96a
lucas <lucaslcslcs1998@gmail.com>
2022-07-26 10:22:41 -0300

    Melhora do App Layout e Letras na Tela

--- app/src/main/java/com/dio/calculadoraimcapp/MainActivity.kt
+++ app/src/main/java/com/dio/calculadoraimcapp/MainActivity.kt
@@ -1,53 +1,76 @@
 package com.dio.calculadoraimcapp
 
+import android.graphics.Color
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
-import android.view.View
-import androidx.core.widget.doAfterTextChanged
-import androidx.core.widget.doOnTextChanged
-import android.widget.Toast
+import com.dio.calculadoraimcapp.databinding.ActivityMainBinding
 import kotlinx.android.synthetic.main.activity_main.*
 
 
 class MainActivity : AppCompatActivity() {
 
+    private lateinit var binding: ActivityMainBinding
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
         setListeners()
     }
 
-    private fun setListeners() {
-        alturaEDT?.doAfterTextChanged { text ->
-            //Toast.makeText(this, text.toString(), Toast.LENGTH_SHORT).show()
+    fun setListeners() {
+        binding.calcularBTN.setOnClickListener {
+            calcularIMC(
+                binding.pesoEDT.text.toString(),
+                binding.alturaEDT.text.toString()
+            )
         }
-
-        pesoEDT?.doOnTextChanged { text, _, _, _ ->
-            //titleTXT?.text = text
-        }
-        calcularBTN?.setOnClickListener {
-        calcularIMC(pesoEDT.text.toString(), alturaEDT.text.toString())}
     }
 
     private fun calcularIMC(peso: String, altura: String) {
         val peso = peso.toFloatOrNull()
         val altura = altura.toFloatOrNull()
 
-        if (peso != null && altura != null){
+        if (peso != null && altura != null) {
             val imc = peso / (altura * altura)
             var result = ""
-            if (imc >= 25.0) {
-                result = "Acima do Peso"
-                pesoacima.text = result
+            if (imc >= 40.0) {
 
-            } else if (imc < 18.5) {
+                result = "Obesidade Grau III"
+                binding.textIMC.setTextColor(Color.parseColor("#D50000"))
+                textIMC.text = result
+
+            } else if (imc >= 35.0) {
+
+                result = "Obesidade Grau II"
+                binding.textIMC.setTextColor(Color.parseColor("#B71C1C"))
+                textIMC.text = result
+
+            } else if (imc >= 30.0) {
+                result = "Obesidade Grai I"
+                binding.textIMC.setTextColor(Color.parseColor("#D32F2F"))
+                textIMC.text = result
+            } else if (imc >= 25.0) {
+
+                result = "Sobrepeso"
+                binding.textIMC.setTextColor(Color.parseColor("#F44336"))
+                textIMC.text = result
+
+            } else if (imc >= 18.5) {
+
+                result = "Peso Normal"
+                binding.textIMC.setTextColor(Color.parseColor("#689F38"))
+                textIMC.text = result
+
+            } else if (imc <= 18.5) {
+
                 result = "Abaixo do Peso"
-                pesoabaixo.text = result
-            } else {
-                result = "Peso Ideal"
-                pesoideal.text = result
+                binding.textIMC.setTextColor(Color.parseColor("#FF9800"))
+                textIMC.text = result
             }
-            titleTXT.text = "IMC: %.2f" .format(imc);
+
+            titleTXT.text = "IMC: %.2f".format(imc)
         }
 
     }
